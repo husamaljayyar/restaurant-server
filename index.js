388641397fff27c1544ab95b18e649912b843e42
hussam-ALJayyar <hosam.algayyar@gmail.com>
2023-05-16 01:26:03 +0300

    resolve error in route server

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-
 const jsonServer = require("json-server");
+const auth = require("json-server-auth");
 const server = jsonServer.create();
 const router = jsonServer.router("./db.json");
-const auth = require("json-server-auth");
 const middlewares = jsonServer.defaults({
   static: "./build",
 });
 const PORT = process.env.PORT || 5000;
 server.use(middlewares);
-server.use(
-  jsonServer.rewriter({
-    "/api/*": "/$1",
-  })
-);
 
 server.db = router.db;
 
