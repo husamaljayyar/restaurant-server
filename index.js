10fd0f01523f876e3e37ebd53e2622e0fac570f0
hussam-ALJayyar <hosam.algayyar@gmail.com>
2023-05-16 00:52:00 +0300

    server update

--- index.js
+++ index.js
@@ -21,33 +21,3 @@ server.use(router);
 server.listen(PORT, () => {
   console.log("Server is running");
 });
-
-
-
-
-
-
-
-
-
-/* const jsonServer = require('json-server')
-const auth = require('json-server-auth')
-const app = jsonServer.create()
-const router = jsonServer.router('db.json')
- */
-// Bind the router db to the app
-// app.db = router.db
-
-/* const rules = auth.rewriter({
-  // Permission rules
-  users: 600,
-  // Other rules
-  "/api/*": "/$1"
-})
-
-// You must apply the auth middleware before the router
-app.use(rules) */
-
-/* app.use(auth)
-app.use(router)
-app.listen(5000) */
