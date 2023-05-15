274b324124b5124840ccb64955fea062e46dac04
hussam-ALJayyar <hosam.algayyar@gmail.com>
2023-05-16 00:41:39 +0300

    server update

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
-const jsonServer = require('json-server')
+
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("./db.json");
+const auth = require("json-server-auth");
+const middlewares = jsonServer.defaults({
+  static: "./build",
+});
+const PORT = process.env.PORT || 5000;
+server.use(middlewares);
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+  })
+);
+
+server.db = router.db;
+
+server.use(auth);
+server.use(router);
+server.listen(PORT, () => {
+  console.log("Server is running");
+});
+
+
+
+
+
+
+
+
+
+/* const jsonServer = require('json-server')
 const auth = require('json-server-auth')
 const app = jsonServer.create()
 const router = jsonServer.router('db.json')
-
+ */
 // Bind the router db to the app
-app.db = router.db
+// app.db = router.db
 
-const rules = auth.rewriter({
+/* const rules = auth.rewriter({
   // Permission rules
   users: 600,
   // Other rules
@@ -14,8 +46,8 @@ const rules = auth.rewriter({
 })
 
 // You must apply the auth middleware before the router
-app.use(rules)
+app.use(rules) */
 
-app.use(auth)
+/* app.use(auth)
 app.use(router)
-app.listen(5000)
+app.listen(5000) */
